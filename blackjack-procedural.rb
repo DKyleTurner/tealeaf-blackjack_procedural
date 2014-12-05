fe3a4050113dff4456e2121929c58837d9c3128e
Kyle Turner <turnerdkyle@icloud.com>
2014-12-04 18:11:00 -0700

    Working on changing string values of face cards to integers

--- blackjack-procedural.rb
+++ blackjack-procedural.rb
@@ -1,26 +1,44 @@
-###################################################################################################
-player_cards = []
-dealer_cards = []
+require 'pry'
 
-player_card_total = ""
-dealer_card_total = 0
+def deal_random_card(suited_cards, player_cards)
+  suited_cards.each_pair do |k, v|
+    v.sample
+    k.sample
+    player_cards.push(v.sample)
+  end
+end
 
-dealer_hits = false
-dealer_stays = false
-
-###################################################################################################
-
-###################################################################################################
-# PICKS A RANDOM 'FACE' AND 'SUIT' - HOW TO REMOVE EACH CARD AFTER IT IS USED?
 card_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'king', 'queen', 'jack', 'ace']
 card_suits = ['hearts', 'diamonds', 'clubs', 'spades']
 
 suited_cards = {
-    card_suits => card_values,
+    card_suits => card_values
 }
 
-suited_cards.each_pair do |k, v|
-  puts "#{v.sample} of #{k.sample}"
+puts "Would you like to play Blackjack? (y, n)"
+user_decision = gets.chomp
+
+player_cards = []
+
+if user_decision.downcase == 'y'
+  2.times{deal_random_card(suited_cards, player_cards)}
+  puts player_cards
+  begin
+    puts 'Hit or Stay?'
+    hit_or_stay = gets.chomp
+    if hit_or_stay.downcase == 'hit'
+      deal_random_card(suited_cards, player_cards)
+
+      # TRYING TO SWITCH THE STRINGS IN 'suited_cards' TO THE INTEGER 10
+      # player_cards.each do |val|
+      #   if val.instance_of?(String)
+      #     val == 10
+      #   end
+      # end
+      puts player_cards
+      binding.pry
+    end
+  end until hit_or_stay.downcase == 'stay'
 end
 
 ###################################################################################################
@@ -29,11 +47,11 @@ end
 
 def check_for_winner(player_sum, dealer_sum)
   if player_sum == 21 && dealer_sum < 21
-    return "You win!"
+     "You win!"
   elsif player_sum == 21 && dealer_sum == 21
-    return "It's a tie!"
+     "It's a tie!"
   elsif player_sum < 21 && dealer_sum == 21
-    return "The dealer won..."
+     "The dealer won..."
   end
 end
 
